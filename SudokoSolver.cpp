c1e68df729633b3dc0fbd4def69e8d428f78bc2d
VaibhavMandloi <vaibhavmandloi2208@gmail.com>
2025-02-19 16:53:09 +0530

    Example sudoko solved succefully

---
+++ SudokoSolver.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#define N 9
+using namespace std;
+
+// Function to check if it's safe to place num in grid[row][col]
+bool isSafe(int grid[N][N], int row, int col, int num) {
+    for (int x = 0; x < N; x++) {
+        if (grid[row][x] == num || grid[x][col] == num) {
+            return false;
+        }
+    }
+    
+    int startRow = row - row % 3, startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Function to solve the Sudoku using backtracking
+bool solveSudoku(int grid[N][N]) {
+    int row, col;
+    bool emptySpot = false;
+    for (row = 0; row < N; row++) {
+        for (col = 0; col < N; col++) {
+            if (grid[row][col] == 0) {
+                emptySpot = true;
+                break;
+            }
+        }
+        if (emptySpot) break;
+    }
+    
+    if (!emptySpot) return true; // If no empty space left, puzzle solved
+
+    for (int num = 1; num <= 9; num++) {
+        if (isSafe(grid, row, col, num)) {
+            grid[row][col] = num;
+            if (solveSudoku(grid)) return true;
+            grid[row][col] = 0; // Backtrack
+        }
+    }
+    return false;
+}
+
+// Function to print the Sudoku grid
+void printGrid(int grid[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int grid[N][N] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+    
+    if (solveSudoku(grid))
+        printGrid(grid);
+    else
+        cout << "No solution exists" << endl;
+    
+    return 0;
+}
